93ec0068200118adcc90ef9518713bb8d75d77bc
Jade Casillano <casillanojade123@gmail.com>
2024-02-23 14:52:09 +0800

    TryTest

--- backend/APP/Controllers/taskController.js
+++ backend/APP/Controllers/taskController.js
@@ -1,86 +1,149 @@
-const mongoose = require("mongoose");
-const asyncHandler = require("express-async-handler");
+// const mongoose = require("mongoose");
+const Task = require("../Models/task_model");
 
-const Task = require("../models/taskModel");
-
-const getTasks = asyncHandler(async (req, res) => {
+const getTasks = async (req, res) => {
   const tasks = await Task.find();
 
   res.status(200).json({ tasks });
-});
+};
 
-const createTask = asyncHandler(async (req, res) => {
-  const { title, description, priorityLevel, assingee, dueDate, status } =
+const createTask = async (req, res) => {
+  const { title, description, priorityLevel, assignee, dueDate, status } =
     req.body;
 
-  const task = new Task({
-    title: title,
-    description: description,
-    priorityLevel: priorityLevel,
-    assingee: assingee,
-    dueDate: dueDate,
-    status: status,
-  });
-
-  task
-    .save()
-    .then((result) => {
-      res.status(200).send({
-        successful: true,
-        message: `Succesfully added Task: ${result.title}`,
-      });
-    })
-    .catch((error) => {
-      res.status(500).send({
-        successful: false,
-        message: error.message,
-      });
-    });
-});
-
-const updateTask = asyncHandler(async (req, res) => {
   try {
-    const { title, description, priorityLevel, assingee, dueDate, status } = req.body;
-    let task = await Task.findOneAndUpdate({ _id: req.params.id });
-    if (task === null) {
-      res.status(404).send({
-        successful: false,
-        message: "Task does not exist",
-      });
-    } else {
-      task = {
-        title: title,
-        description: description,
-        priorityLevel: priorityLevel,
-        assingee: assingee,
-        dueDate: dueDate,
-        status: status,
-      };
-      task
-        .save()
-        .then((result) => {
-          res.status(200).send({
-            successful: true,
-            message: "Successfully updated Task.",
-          });
-        })
-        .catch((err) => {
-          res.status(500).send({
-            successful: false,
-            message: err.message,
-          });
-        });
-    }
+    const task = new Task({
+      title,
+      description,
+      priorityLevel,
+      assignee,
+      dueDate,
+      status,
+    });
+
+    const savedTask = await task.save();
+
+    res.status(201).send({
+      successful: true,
+      message: `Successfully added Task: ${savedTask.title}`,
+      task: savedTask,
+    });
+  } catch (error) {
+    res.status(500).send({
+      successful: false,
+      message: error.message,
+    });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { title, description, priorityLevel, assignee,status, dueDate } = req.body;
+
+    const updatedTask = await Task.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        title,
+        description,
+        priorityLevel,
+        assignee,
+        dueDate,
+        status
+      },
+      { new: true }
+    );
+    
+    handleTaskMethod(res, updatedTask, "updated");
   } catch (err) {
     res.status(500).send({
       successful: false,
       message: err.message,
     });
   }
-});
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete({ _id: req.params.id });
+
+    handleTaskMethod(res, deletedTask, "deleted");
+  } catch (err) {
+    res.status(500).send({
+      successful: false,
+      message: err.message,
+    });
+  }
+};
+
+const startTask = async (req, res) => {
+  try {
+    const startedTask = await Task.findOneAndUpdate(
+      { _id: req.params.id },
+      { status: "In Progress" }
+    );
+    
+    handleTaskMethod(res, startedTask, "started");
+  } catch (err) {
+    res.status(500).send({
+      successful: false,
+      message: err.message,
+    });
+  }
+};
+
+const todoTask = async (req, res) => {
+  try {
+    const todo = await Task.findOneAndUpdate(
+      { _id: req.params.id },
+      { status: "To-do" }
+    );
+    
+    handleTaskMethod(res, todo, "to-do");
+  } catch (err) {
+    res.status(500).send({
+      successful: false,
+      message: err.message,
+    });
+  }
+};
+
+const completeTask = async (req, res) => {
+  try {
+    const completedTask = await Task.findOneAndUpdate(
+      { _id: req.params.id },
+      { status: "Completed" }
+    );
+
+    handleTaskMethod(res, completedTask, "completed");
+  } catch (err) {
+    res.status(500).send({
+      successful: false,
+      message: err.message,
+    });
+  }
+};
+
+async function handleTaskMethod(res, action, str) {
+  if (!action) {
+    res.status(404).send({
+      successful: false,
+      message: "Task does not exist",
+    });
+  } else {
+    res.status(200).send({
+      successful: true,
+      message: `Successfully ${str} Task.`,
+      action,
+    });
+  }
+}
 
 module.exports = {
   getTasks,
   createTask,
   updateTask,
+  deleteTask,
+  startTask,
+  todoTask,
+  completeTask,
 };
