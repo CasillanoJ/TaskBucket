a84f04b6a2084104a1bb77015abde7658f7a937b
DESKTOP-61ONS9D\ledes <ledesmajack3@gmail.com>
2024-02-25 19:42:42 +0800

    changepassword

--- backend/APP/Controllers/userController.js
+++ backend/APP/Controllers/userController.js
@@ -1,95 +1,130 @@
 const User = require('../Models/user_model');
-
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 const addUser = async (req, res, next) => {
   const saltRound = 10;
 
   try {
-    
-    let{first_name, last_name, email, password, isAdmin, isVerified} = req.body
-   
-  
-    const checkUser = await User.findOne({email: email})
+    let { first_name, last_name, email, password, isAdmin, isVerified } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password,saltRound)
+    const checkUser = await User.findOne({ email: email });
 
-    if(checkUser == null){
+    const hashedPassword = await bcrypt.hash(password, saltRound);
 
+    if (checkUser == null) {
       const newUser = new User({
         first_name: first_name,
         last_name: last_name,
         email: email,
         password: hashedPassword,
         isAdmin: isAdmin,
-        isVerified: isVerified
-    })
+        isVerified: isVerified,
+      });
 
-   await newUser.save().then((result)=>{
-    res.status(200).send({
-        successful: true,
-        message:`Succesfully added User ${result.first_name} ${result.last_name}`,
-        id: result._id
-    })
-
-   }).catch((error)=>{
-        res.status(500).send({
-            successful: false,
-            message: error.message
+      await newUser
+        .save()
+        .then((result) => {
+          res.status(200).send({
+            successful: true,
+            message: `Successfully added User ${result.first_name} ${result.last_name}`,
+            id: result._id,
+          });
         })
-   })
-    }else{
+        .catch((error) => {
+          res.status(500).send({
+            successful: false,
+            message: error.message,
+          });
+        });
+    } else {
       res.status(400).send({
         successful: false,
-        message:`Email is already taken`,
-      
-    })
+        message: `Email is already taken`,
+      });
     }
   } catch (error) {
     res.status(500).send({
       successful: false,
-      message: error.message
-  })
+      message: error.message,
+    });
   }
-   
 };
 
-
-
-const getAllUsers = async (req, res, next)=>{
-
-  try{
-      const users = await User.find()
-      if(users.length != 0){
-          res.status(200).json({
-              successful: true,
-              message: "Succesfully retrieved User details.",
-              count: users.length,
-               data: users
-      })
-    }else{
+const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    if (users.length != 0) {
       res.status(200).json({
-          successful: true,
-          message: "No User data yet",
-          count: users.length    
-  })
+        successful: true,
+        message: 'Successfully retrieved User details.',
+        count: users.length,
+        data: users,
+      });
+    } else {
+      res.status(200).json({
+        successful: true,
+        message: 'No User data yet',
+        count: users.length,
+      });
     }
-  }catch(error){
-      res.status(500).send({
-          successful: false,
-          message: error.message
-      })
+  } catch (error) {
+    res.status(500).send({
+      successful: false,
+      message: error.message,
+    });
   }
-
 };
 
+const changePassword = async (req, res, next) => {
+  const saltRound = 10;
 
+  try {
+    const { email, oldPassword, newPassword, confirmPassword } = req.body;
 
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({
+        successful: false,
+        message: 'New password and confirm password do not match',
+      });
+    }
 
+    const user = await User.findOne({ email });
 
+    if (!user) {
+      return res.status(404).json({
+        successful: false,
+        message: 'User not found',
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        successful: false,
+        message: 'Invalid old password',
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, saltRound);
+
+    user.password = hashedPassword;
+    await user.save();
+
+    return res.status(200).json({
+      successful: true,
+      message: 'Password changed successfully',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      successful: false,
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   addUser,
-  getAllUsers
-
-}
+  getAllUsers,
+  changePassword,
+};
--- backend/APP/Routers/user_routes.js
+++ backend/APP/Routers/user_routes.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const {addUser, getAllUsers} = require('../Controllers/userController');
+const {
+  addUser,
+  getAllUsers,
+  changePassword,
+} = require('../Controllers/userController');
 const router = express.Router();
+const {
+  getUserTask,
+  getTaskAdmin,
+} = require('../Controllers/userTaskController');
 
-const {getUserTask, getTaskAdmin} = require('../Controllers/userTaskController')
-
-
-router.post('/add', addUser)
-
-router.get('/', getAllUsers)
-
-router.get('/getTask/:id',getUserTask)
-
-
-
+router.post('/add', addUser);
+router.get('/', getAllUsers);
+router.get('/getTask/:id', getUserTask);
 // For testing Only need to fix
-router.get('/getTaskAdmin', getTaskAdmin)
-
+router.get('/getTaskAdmin', getTaskAdmin);
+router.post('/change-password', changePassword);
 
 module.exports = router;
