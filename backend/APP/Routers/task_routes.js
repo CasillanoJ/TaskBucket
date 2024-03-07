2dc3579946aa4b499a56f38b09f8f7040c1b7cab
DESKTOP-61ONS9D\ledes <ledesmajack3@gmail.com>
2024-03-07 13:35:07 +0800

    pa fix boi getallbyuserdaterange tas historylogs salamuch

--- backend/APP/Routers/task_routes.js
+++ backend/APP/Routers/task_routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getTasks, createTask, updateTask } = require('../Controllers/taskController');
+const { getTasks, createTask, updateTask,getTotalcompletedofuser,getHistoryLogs} = require('../Controllers/taskController');
 
 router.route("/").get(getTasks).post(createTask);
-router.put("/:id", updateTask)
+router.put("/:id", updateTask);
+router.route("/completed/:userId").get(getTotalcompletedofuser);
+router.get("/logs", getHistoryLogs); 
+
 
 module.exports = router;
